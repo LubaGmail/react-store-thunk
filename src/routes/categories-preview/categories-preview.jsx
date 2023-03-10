42d1024bc5519500180c344528500dd17bd487ec
LL Powe <powe2009@gmail.com>
2023-03-10 16:21:47 -0500

    abc

--- src/routes/categories-preview/categories-preview.jsx
+++ src/routes/categories-preview/categories-preview.jsx
@@ -31,7 +31,6 @@ const CategoriesPreview = () => {
                             isLoading ? (
                                 <>
                                     <Spinner /> 
-                                    'ABC'
                                 </>
                                
                             ): (
